92b0b71ade826a9fd86a21be361624a2794dd45f
Krystian Trojanowski <ktrojanowski@hso.grupa.skok>
2018-01-01 18:32:55 +0100

    Lag mechanics

--- src/mechanics/lag/index.js
+++ src/mechanics/lag/index.js
@@ -14,89 +14,153 @@ const manaCurve = {
     7: 1.00,
     8: 1.00,
     9: 0.95,
-    10: 0.95
+    10: 0.95,
+    12: 0.95,
+    20: 0.95
 }
 
 const cardTarget = {
-    any: { multiplier: 70, value: 1 },
-    enemy: { multiplier: 70, value: 1 },
-    friendly: { multiplier: 70, value: 1 },
-    character: { multiplier: 70, value: 1 },
-    characters: { multiplier: 70, value: 1 },
-    hero: { multiplier: 70, value: 1 },
-    minion: { multiplier: 70, value: 1 },
-    minions: { multiplier: 70, value: 1 },
-    all: { multiplier: 70, value: 1 },
-    randomly: { multiplier: 70, value: 1 },
-    your: { multiplier: 70, value: 1 },
-    this: { multiplier: 70, value: 1 },
+    any: { multiplier: 1 },
+    enemy: { multiplier: 1.2 },
+    friendly: { multiplier: 1.2 },
+    character: { multiplier: 1.1 },
+    characters: { multiplier: 0.9 },
+    hero: { multiplier: 0.8 },
+    minion: { multiplier: 1.1 },
+    minions: { multiplier: 1.2 },
+    all: { multiplier: 1.2 },
+    randomly: { multiplier: 0.8 },
+    your: { multiplier: 1 },
+    //this: { multiplier: 70, value: 1 },
 }
 
+
 const mechanics = {
-    adapt: { name: "Adapt", multiplier: 70, value: 1 },
-    battlecry: { name: "Battlecry", multiplier: 70, value: 1 },
-    charge: { name: "Charge", multiplier: 70, value: 1 },
-    chooseOne: { name: "Choose One", multiplier: 70, value: 1 },
-    combo: { name: "Combo", multiplier: 70, value: 1 },
-    counter: { name: "Counter", multiplier: 70, value: 1 },
-    deathrattle: { name: "Deathrattle", multiplier: 70, value: 1 },
-    discover: { name: "Discover", multiplier: 70, value: 1 },
-    divineShield: { name: "Divine Shield", multiplier: 70, value: 1 },
-    enrage: { name: "Enrage", multiplier: 70, value: 1 },
-    freeze: { name: "Freeze", multiplier: 70, value: 1 },
-    immune: { name: "Immune", multiplier: 70, value: 1 },
-    inspire: { name: "Inspire", multiplier: 70, value: 1 },
-    lifesteal: { name: "Lifesteal", multiplier: 70, value: 1 },
-    megaWindfury: { name: "Mega Windfury", multiplier: 70, value: 1 },
-    overload: { name: "Overload", multiplier: 70, value: 1 },
-    poisonous: { name: "Poisonous", multiplier: 70, value: 1 },
-    quest: { name: "Quest", multiplier: 70, value: 1 },
-    secret: { name: "Secret", multiplier: 70, value: 1 },
-    silence: { name: "Silence", multiplier: 70, value: 1 },
-    stealth: { name: "Stealth", multiplier: 70, value: 1 },
-    spellDamage: { name: "Spell Damage", multiplier: 70, value: 1 },
-    taunt: { name: "Taunt", multiplier: 70, value: 1 },
-    windfury: { name: "Windfury", multiplier: 70, value: 1 }
+    adapt: { name: "Adapt", multiplier: 150, value: 1 },
+    battlecry: { name: "Battlecry", multiplier: 50, value: 1 },
+    charge: { name: "Charge", multiplier: 200, value: 1 },
+    combo: { name: "Combo", multiplier: 50, value: 1 },
+    counter: { name: "Counter", multiplier: 200, value: 1 },
+    deathrattle: { name: "Deathrattle", multiplier: 130, value: 1 },
+    discover: { name: "Discover", multiplier: 150, value: 1 },
+    divineShield: { name: "Divine Shield", multiplier: 100, value: 1 },
+    enrage: { name: "Enrage", multiplier: 80, value: 1 },
+    freeze: { name: "Freeze", multiplier: 50, value: 1 },
+    immune: { name: "Immune", multiplier: 90, value: 1 },
+    inspire: { name: "Inspire", multiplier: 120, value: 1 },
+    lifesteal: { name: "Lifesteal", multiplier: 110, value: 1 },
+    megaWindfury: { name: "Mega Windfury", multiplier: 200, value: 1 },
+    overload: { name: "Overload", multiplier: 30, value: 1 },
+    poisonous: { name: "Poisonous", multiplier: 150, value: 1 },
+    quest: { name: "Quest", multiplier: 200, value: 1 },
+    secret: { name: "Secret", multiplier: 100, value: 1 },
+    silence: { name: "Silence", multiplier: 80, value: 1 },
+    stealth: { name: "Stealth", multiplier: 80, value: 1 },
+    spellDamage: { name: "Spell Damage", multiplier: 60, value: 1 },
+    taunt: { name: "Taunt", multiplier: 100, value: 1 },
+    windfury: { name: "Windfury", multiplier: 90, value: 1 }
 }
 
 const textMechanics = {
-    cardDraw: { multiplier: 70, value: 1 },
-    castSpell: { multiplier: 70, value: 1 },
-    copy: { multiplier: 70, value: 1 },
+    cardDrawSingle: { multiplier: 100, value: 1 },
+    cardDrawNumber: { multiplier: 100, value: 1 },
+
+    castSpell: { multiplier: 50, value: 1 },
+
+    chooseOne: { multiplier: 50, value: 1 },
+
+    copy: { multiplier: 200, value: 1 },
+
     dealDamage: { multiplier: 70, value: 1 },
-    destroy: { multiplier: 70, value: 1 },
-    disableHeroPower: { multiplier: 70, value: 1 },
-    discard: { multiplier: 70, value: 1 },
-    enchant: { multiplier: 70, value: 1 },
-    elusive: { multiplier: 70, value: 1 },
-    equip: { multiplier: 70, value: 1 },
-    forgetful: { multiplier: 70, value: 1 },
+    dealDamageEqual: { multiplier: 140, value: 1 },
+    dealDamageRandom: { multiplier: 60, value: 1, value2: 1},
+    dealDamageRaise: { multiplier: 210, value: 1 },
+
+    destroy: { multiplier: 250, value: 1 },
+    destroyNumber: { multiplier: 250, value: 1 },
+
+    disableHeroPower: { multiplier: 100, value: 1 },
+
+    discardSingle: { multiplier: -100, value: 1 },
+    discardTwo: { multiplier: -100, value: 2 },
+    discardNumber: { multiplier: -100, value: 1 },
+    discardHand: { multiplier: -300, value: 1 },
+
+    enchantDouble: { multiplier: 100, value: 1 },
+    enchantAttack: { multiplier: 70, value: 1 },
+    enchantHealth: { multiplier: 90, value: 1 },
+
+    elusive: { multiplier: 130, value: 1 },
+
+    equip: { multiplier: 60, value: 1 },
+
+    forgetful: { multiplier: -100, value: 1 },
+
     gainArmor: { multiplier: 70, value: 1 },
-    generate: { multiplier: 70, value: 1 },
-    incrementAttribute: { multiplier: 70, value: 1 },
-    joust: { multiplier: 70, value: 1 },
-    mindControlEffect: { multiplier: 70, value: 1 },
-    modifyCost: { multiplier: 70, value: 1 },
-    multiplyAttribute: { multiplier: 70, value: 1 },
-    noDurabilityLoss: { multiplier: 70, value: 1 },
-    permanent: { multiplier: 70, value: 1 },
-    putIntoBattlefield: { multiplier: 70, value: 1 },
-    putIntoHand: { multiplier: 70, value: 1 },
-    refreshMana: { multiplier: 70, value: 1 },
-    removeFromDeck: { multiplier: 70, value: 1 },
-    replace: { multiplier: 70, value: 1 },
-    restoreHealth: { multiplier: 70, value: 1 },
-    return: { multiplier: 70, value: 1 },
-    setAttribute: { multiplier: 70, value: 1 },
-    shuffleIntoDeck: { multiplier: 70, value: 1 },
-    spendMana: { multiplier: 70, value: 1 },
-    summon: { multiplier: 70, value: 1 },
-    transform: { multiplier: 70, value: 1 },
-    transformInHand: { multiplier: 70, value: 1 },
-    unlimitedAttacks: { multiplier: 70, value: 1 }
+    gainArmorNumber: { multiplier: 70, value: 1 },
+    gainArmorMuch: { multiplier: 140, value: 1 },
+    gainArmorEqual: { multiplier: 140, value: 1 },
+
+    generate: { multiplier: 250, value: 1 },
+
+    incrementAttributeDouble: { multiplier: 80, value: 1, value2: 1 },
+    incrementAttributeAttack: { multiplier: 70, value: 1 },
+    incrementAttributeHealth: { multiplier: 90, value: 1 },
+    incrementAttributeDurability: { multiplier: 90, value: 1 },
+    incrementAttributeMana: { multiplier: 70, value: 1 },
+
+    joust: { multiplier: 80, value: 1 },
+
+    mindControlEffect: { multiplier: 130, value: 1 },
+
+    modifyCostLess: { multiplier: 100, value: 1 },
+    modifyCostMore: { multiplier: 100, value: 1 },
+
+    multiplyAttribute: { multiplier: 130, value: 1 },
+
+    noDurabilityLoss: { multiplier: 120, value: 1 },
+
+    //permanent: { multiplier: 70, value: 1 },
+
+    putIntoBattlefield: { multiplier: 150, value: 1 },
+
+    putIntoHand: { multiplier: 150, value: 1 },
+    
+
+    reduce: { multiplier: 170, value: 1 },
+
+    refreshMana: { multiplier: 250, value: 1 },
+
+    removeFromDeck: { multiplier: 100, value: 1 },
+
+    replace: { multiplier: 250, value: 1 },
+
+    restoreHealth: { multiplier: 90, value: 1 },
+
+    return: { multiplier: 100, value: 1 },
+
+    setAttributeHealth: { multiplier: 210, value: 1 },
+    setAttributeAttack: { multiplier: 180, value: 1 },
+
+    shuffleIntoDeck: { multiplier: 250, value: 1 },
+
+    spendMana: { multiplier: 200, value: 1 },
+
+    summonMinionDouble: { multiplier: 80, value: 1, value2: 1 },
+    summonMinionCost: { multiplier: 100, value: 1 },
+    summonMinionCopy: { multiplier: 150, value: 1 },
+
+    transformDouble: { multiplier: 80, value: 1, value2: 1 },
+    transformMore: { multiplier: 150, value: 1 },
+    transformLess: { multiplier: 150, value: 1 },
+
+    transformInHand: { multiplier: 150, value: 1 },
+
+    unlimitedAttacks: { multiplier: 200, value: 1 }
 }
 
 const mechanicsTypes = {
+    //oncontrol
     areaOfEffect: { multiplier: 70, value: 1 },
     inHandEffect: { multiplier: 70, value: 1 },
     onDiscardEffect: { multiplier: 70, value: 1 },
@@ -110,7 +174,7 @@ const mechanicsTypes = {
 
 export default function appraiseCardValue(collection, card) {
 
-    const cardValues = {};
+    let cardValues = {};
     const cardTargets = {};    
 
     let cardScore = 0;
@@ -122,7 +186,7 @@ export default function appraiseCardValue(collection, card) {
 
         if(regex.test(card.text)) {
             cardValues[regexName] = _.cloneDeep(textMechanics[regexName]);
-            regex.exec(card.text)[1] ? cardValues[regexName].value = regex.exec(card.text)[1] : null;
+            regex.exec(card.text)[1] ? cardValues[regexName].value = Number(regex.exec(card.text)[1]) : null;
         }
 
     });
@@ -141,35 +205,38 @@ export default function appraiseCardValue(collection, card) {
 
     }
 
-    _.each(cardTextMechanicsRegex, (regex, regexName) => {
-
-        if(regex.test(card.text)) {
-            cardValues[regexName] = _.cloneDeep(textMechanics[regexName]);
-            regex.exec(card.text)[1] ? cardValues[regexName].value = regex.exec(card.text)[1] : null;
-        }
-
-    });
-
     // getting card targets
     _.each(cardTargetRegex, (regex, regexName) => {
+
         if(regex.test(card.text)) {
             cardTargets[regexName] = cardTarget[regexName];
         }
 
     });
 
+    // handle choose one 
+    if(!_.isEmpty(cardValues.chooseOne)) {
+        let max = _.sortBy(cardValues, [function(o) { return -(o.multiplier * o.value); }]);
+        cardValues = { bestChooseOne : max.shift() }
+    }
+
 
     // sumup all values 
-
     _.each(cardValues, (value, key) => {
-
         cardScore+= value.multiplier * value.value;
-
     });
-
 
     console.log(cardValues, cardTargets);
 
-    return Math.round(cardScore / ( (card.cost + 1)  * (manaCurve[card.cost])).toFixed(0));
+    switch(card.type) {
+        case "Weapon": return  Math.round(( ((card.durability * 60 ) + (card.attack * 70) + cardScore ) / (card.cost + 1) * (manaCurve[card.cost]))* 0.4).toFixed(0);
+        case "Spell": {
+        
+            return Math.round( cardScore / ( (card.cost + 1)  * (manaCurve[card.cost])).toFixed(0));
+        }
+        case "Minion": return Math.round(( cardScore + ( card.attack * 35 ) + (card.health * 40) / (card.cost + 1) * (manaCurve[card.cost])) * 0.2).toFixed(0);
+        default: return 0;
+    }
+
 };
 
--- src/mechanics/lag/regex/mechanicRegex.js
+++ src/mechanics/lag/regex/mechanicRegex.js
@@ -1,41 +1,101 @@
 export const cardTextMechanicsRegex = {
-    // dealsDamage: /.*\bdeals damage\b.*/i,
     // dealItsDamage: /.*\bdeal its damage\b.*/i,
     // dealThatMuchDamage: /.*\bdeal that much damage\b.*/i,
-    // dealRandom: /.*\bdeal random\b.*/i,
 
-    cardDraw: /.*\bdraw\b.*/i,
-    castSpell: /.*\bcast\b.*/i,
+    cardDrawSingle: /.*draws? an?.*\b/i,
+    cardDrawNumber: /.*draws? (\d\d?) .*/i,
+
+    castSpell: /.*\bcasts?\b.*/i,
+
+    chooseOne: /.*choose one.*/i,
+
     copy: /.*\bcopy\b.*/i,
-    dealDamage: /.*deals? \$?(\d*) (?:extra|damage).*/i,
-    destroy: /.*\bdestroy\b.*/i,
+
+    dealDamage: /.*deals? \$?(\d*) (?:extra|damages?).*/i,
+    dealDamageEqual: /deals? damage equal/i,
+    dealDamageRandom: /.*deals? (\d)-(\d) damages?/i,
+    dealDamageRaise: /.*deals? that much damages?/i,
+
+    destroy: /.*\bdestroy \D.*\b/i,
+    destroyNumber: /.*\bdestroy (\d).*\b/i,
+
     disableHeroPower: /.*\bdisabled?\b.*/i,
-    discard: /.*\bdiscard\b.*/i,
-    enchant: /.*\benchant\b.*/i,
+
+    discardSingle: /.*\bdiscard a\b.*/i,
+    discardTwo: /.*\bdiscard two\b.*/i,
+    discardNumber: /.*\bdiscard (\d\d?)\b.*/i,
+    discardHand: /.*discard your.*/i,
+
+    enchantDouble: /.*give.*\+(\d)\/\+(\d).*/i,
+    enchantAttack: /.*gives?.*\+?\-?(\d) attack.*/i,
+    enchantHealth: /.*gives?.*\+?\-?(\d) health.*/i,
+
     elusive: /.*\bbe targeted\b.*/i,
-    equip: /.*\bequip\b.*/i,
+
+    equip: /.*\bequip \b.*/i,
+
     forgetful: /.*\bwrong\b.*/i,
+
     gainArmor: /.*\bgain \d? armor\b.*/i,
-    generate: /.*\bgenerate\b.*/i,
-    incrementAttribute: /.*\bgive\b.*/i,
+    gainArmorNumber: /.*(\d\d?) armor.*/i,
+    gainArmorMuch: /.*much armor.*/i,
+    gainArmorEqual: /.*armor equals?.*/i,
+
+    //??
+    generate: /.*\badd\b.*/i,
+
+    incrementAttributeDouble: /.*gains?.*\+(\d)\/\+(\d).*/i,
+    incrementAttributeAttack:/.*gains?.*\+(\d) attack.*/i,
+    incrementAttributeHealth:/.*gain?.*\+(\d) health.*/i,
+    incrementAttributeMana:/.*gains?.*?(\d?\d?) mana.*/i,
+    incrementAttributeDurability:/.*gain?.*\+(\d) durability.*/i,
+    
     joust: /.*\breveals?\b.*/i,
-    mindControlEffect: /.*\bcontrol\b.*/i,
-    modifyCost: /.*\bgive\b.*/i,
-    multiplyAttribute: /.*\bdouble\b.*/i,
-    noDurabilityLoss: /.*\bgive\b.*/i,
-    permanent: /.*\bpermanent\b.*/i,
-    putIntoBattlefield: /.*\bgive\b.*/i,
-    putIntoHand: /.*\bgive\b.*/i,
-    refreshMana: /.*\brefresh\b.*/i,
-    removeFromDeck: /.*\bremove\b.*/i,
+
+    mindControlEffect: /.*\b(?:take|gain).*control?\b.*/i,
+
+    modifyCostLess: /.*\((\d)\) less.*/i,
+    modifyCostMore: /.*\((\d)\) more.*/i,
+
+    multiplyAttribute: /.*\b(?:double|twice)\b.*/i,
+
+    noDurabilityLoss: /.*\bdoesn't lose durability\b.*/i,
+
+    //permanent: /.*\bpermanent\b.*/i,
+
+    putIntoBattlefield: /.*\bputs?.*battlefield.*\b/i,
+
+    putIntoHand: /.*\bputs?.*hand\b.*/i,
+
+    reduce: /.*reduce.*cost by.*(\d).*/i,
+
+    refreshMana: /.*\brefresh.*mana\b.*/i,
+
+    removeFromDeck: /.*\bremove.*deck\b.*/i,
+
     replace: /.*\breplace\b.*/i,
-    restoreHealth: /.*\brestore\b.*/i,
+
+    restoreHealth: /.*\brestore \#?(\d\d?)\b.*/i,
+
     return: /.*\breturn\b.*/i,
-    setAttribute: /.*\bset\b.*/i,
+
+    setAttributeHealth: /.*\bset.*health to (\d)\b.*/i,
+    setAttributeAttack: /.*\bset.*attack to (\d)\b.*/i,
+
     shuffleIntoDeck: /.*\bshuffle\b.*/i,
+
     spendMana: /.*\bspends?\b.*/i,
-    summon: /.*\bsummons?\b.*/i,
-    transform: /.*\btransorms?\b.*/i,
-    transformInHand: /.*\bgive\b.*/i,
+
+    summonMinionDouble: /.*summons? a?n?.*(\d)\/(\d).*/i,
+    summonMinionCost: /.*summons? a?n?.*(\d)-cost.*/i,
+    summonMinionCopy: /.*summons? a?n?.*(?:copy|copies).*/i,
+
+    transformDouble: /.*\btransforms? into a?n?.*(\d)\/(\d)\b.*/i,
+    transformMore: /.*transorms?.*(\d).*more.*/i,
+    transformLess: /.*transforms?.*(\d).*less.*/i,
+    
+    transformInHand: /.*transforms? it.*/i,
+
     unlimitedAttacks: /.*\bunlimited\b.*/i
+
 }
